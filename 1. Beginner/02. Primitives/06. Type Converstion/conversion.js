3ac8974e9af00e30bf34fd0cfdaac3bca7f602d0
AmanW-GitHub <amanwaghchoure2004@gmail.com>
2023-12-25 14:37:21 -0500

    Finished 01 and 02 in Beginner

---
+++ 1. Beginner/02. Primitives/06. Type Converstion/conversion.js
@@ -0,0 +1,11 @@
+let money = "56.05";
+
+// 1. Convert (money) to number.
+
+console.log(money);
+console.log(typeof money);
+
+money = parseFloat(money);
+
+console.log(money);
+console.log(typeof money);
